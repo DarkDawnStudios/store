ffeeb4848d159f0800381cb8ce6e9f65a7d9b095
Shinji <shinji@shinji.rocks>
2022-08-19 02:33:48 +0800

    add meta class

--- src/utils/models.py
+++ src/utils/models.py
@@ -5,3 +5,5 @@ from django.utils.translation import gettext_lazy as _
 class DateTimeMixin(models.Model):
     inserted_at = models.DateTimeField(_("Creation date and time"), auto_now_add=True)
     updated_at = models.DateTimeField(_("Update date and time"), auto_now=True)
+    class Meta:
+        abstract = True
